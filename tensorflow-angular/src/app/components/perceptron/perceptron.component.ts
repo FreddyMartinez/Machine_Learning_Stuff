de13b1914275c46545a306909153a0744a3b0b77
FreddyMartinez <fhmartinezq@unal.edu.co>
2020-04-27 20:21:34 -0500

    updating dependencies

--- tensorflow-angular/src/app/components/perceptron/perceptron.component.ts
+++ tensorflow-angular/src/app/components/perceptron/perceptron.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import * as tf from '@tensorflow/tfjs';
+import { Component, OnInit } from "@angular/core";
+import * as tf from "@tensorflow/tfjs";
 
 @Component({
-  selector: 'app-perceptron',
-  templateUrl: './perceptron.component.html',
-  styleUrls: ['./perceptron.component.scss']
+  selector: "app-perceptron",
+  templateUrl: "./perceptron.component.html",
+  styleUrls: ["./perceptron.component.scss"],
 })
 export class PerceptronComponent implements OnInit {
-
   /**
    * These variables are used for tensorflow perceptron
    */
@@ -17,7 +16,14 @@ export class PerceptronComponent implements OnInit {
   linearModel: tf.Sequential;
   public prediction: any;
 
-  xs = [[0.1, 0.1], [0.2, 0.2], [0.9, 0.1], [0.1, 0.9], [0.8, 0.8], [0.9, 0.9]];
+  xs = [
+    [0.1, 0.1],
+    [0.2, 0.2],
+    [0.9, 0.1],
+    [0.1, 0.9],
+    [0.8, 0.8],
+    [0.9, 0.9],
+  ];
   // ys = [0, 0, 1, 1, 1, 1]; // OR Gate
   ys = [0, 0, 0, 0, 1, 1]; // AND Gate
   // ys = [1, 1, 0, 0, 0, 0]; // NOR Gate
@@ -31,30 +37,38 @@ export class PerceptronComponent implements OnInit {
   public myPrediction: number;
   public myRoundedPrediction: number;
 
-
   ngOnInit() {
     this.train();
   }
 
   async train() {
     this.linearModel = tf.sequential();
-    this.linearModel.add(tf.layers.dense({units: 1, inputShape: [2], activation: 'sigmoid', useBias: true}));
+    this.linearModel.add(
+      tf.layers.dense({
+        units: 1,
+        inputShape: [2],
+        activation: "sigmoid",
+        useBias: true,
+      })
+    );
 
     // Prepare the model for training: Specify the loss and the optimizer.
-    this.linearModel.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
+    this.linearModel.compile({ loss: "meanSquaredError", optimizer: "sgd" });
 
     // Training data
     const xs = tf.tensor2d(this.xs);
     const ys = tf.tensor1d(this.ys);
 
     // Train
-    await this.linearModel.fit(xs, ys, {epochs: 5000});
+    await this.linearModel.fit(xs, ys, { epochs: 5000 });
 
-    console.log('model trained!');
+    console.log("model trained!");
   }
 
   predict(): void {
-    const output = this.linearModel.predict(tf.tensor2d([this.inputA, this.inputB], [1, 2])) as any;
+    const output = this.linearModel.predict(
+      tf.tensor2d([this.inputA, this.inputB], [1, 2])
+    ) as any;
     this.prediction = Array.from(output.dataSync())[0];
     this.perceptron();
   }
@@ -63,13 +77,13 @@ export class PerceptronComponent implements OnInit {
    * Manually created Perceptron
    */
 
-   sigmoid(x: number): number {
-     return 1 / (1 + Math.exp(-x));
-   }
+  sigmoid(x: number): number {
+    return 1 / (1 + Math.exp(-x));
+  }
 
-   perceptron(): void {
-     const z = this.inputA * this.w1 + this.inputB * this.w2 + this.b;
-     this.myPrediction = this.sigmoid(z);
-     this.myRoundedPrediction = Math.round(this.myPrediction);
-   }
+  perceptron(): void {
+    const z = this.inputA * this.w1 + this.inputB * this.w2 + this.b;
+    this.myPrediction = this.sigmoid(z);
+    this.myRoundedPrediction = Math.round(this.myPrediction);
+  }
 }
